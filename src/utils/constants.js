9757c4a9d7659b6d9f0cfd740bc4ebc02f8562a3
artem sadomtsev <artem.sadomtsev1@mail.ru>
2024-06-21 17:30:40 +0300

    - finish

--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const config = {
-    inputSelector: ".popup__item",
-    submitButtonSelector: ".popup__button",
-    inactiveButtonClass: "popup__button_inactive",
-    inputErrorClass: "popup__input_type_error",
-    errorClass: "popup__input-error_active",
-  };
